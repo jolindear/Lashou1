0d8f44f1de24f96b66edb7211e7623e9d52030c7
jolindear <aaa5128341@.com>
2017-04-13 16:16:30 +0800

    sad

---
+++ app/src/main/java/forever/hepan/lashou/MainActivity.java
@@ -0,0 +1,59 @@
+package forever.hepan.lashou;
+
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void rotateyAnimRun(final View view)
+    {
+        /*ValueAnimator animator = ValueAnimator.ofFloat(0, mScreenHeight
+                - mBlueBall.getHeight());
+        animator.setTarget(mBlueBall);
+        animator.setDuration(1000).start();*/
+        //----------------------------------------------------------------------------------
+        /*
+        PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat("alpha", 1f,
+                0f, 1f);
+        PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat("scaleX", 1f,
+                0, 1f);
+        PropertyValuesHolder pvhZ = PropertyValuesHolder.ofFloat("scaleY", 1f,
+                0, 1f);
+        ObjectAnimator.ofPropertyValuesHolder(view, pvhX, pvhY,pvhZ).setDuration(1000).start();*/
+        //----------------------------------------------------------------------------------
+        /*
+        ObjectAnimator//
+                .ofFloat(view, "rotationX", 0.0F, 360.0F)//
+                .setDuration(2000)//
+                .start();*/
+        //----------------------------------------------------------------------------------
+       /* ObjectAnimator anim = ObjectAnimator//
+                .ofFloat(view, "zhy", 1.0F,  0.1F)//
+                .setDuration(2000);//
+        anim.start();
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+        {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation)
+            {
+                float cVal = (Float) animation.getAnimatedValue();
+                view.setAlpha(cVal);
+                view.setScaleX(cVal);
+                view.setScaleY(cVal);
+            }
+        });*/
+
+    }
+}
